3028bd800bd033009a96c25575c2346740b1515c
URAYUSHJAIN <urayushjain@gmail.com>
2024-12-14 03:48:00 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Services from "./pages/Services";
 import Impact from "./pages/Impact";
 import UseCase from "./pages/UseCase"; 
